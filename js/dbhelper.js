d9647ff6ab8d7dc0d1c19e3c9c41622aa6ec5418
Ekaterina Karelova <katekarelova@gmail.com>
2018-07-22 15:57:32 +0300

    Responsive design

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -153,11 +153,12 @@ class DBHelper {
     return (`/img/${restaurant.photograph}`);
   }
 
+
   /**
    * Map marker for a restaurant.
    */
    static mapMarkerForRestaurant(restaurant, map) {
-    // https://leafletjs.com/reference-1.3.0.html#marker  
+    // https://leafletjs.com/reference-1.3.0.html#marker
     const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
       {title: restaurant.name,
       alt: restaurant.name,
@@ -165,7 +166,7 @@ class DBHelper {
       })
       marker.addTo(newMap);
     return marker;
-  } 
+  }
   /* static mapMarkerForRestaurant(restaurant, map) {
     const marker = new google.maps.Marker({
       position: restaurant.latlng,
@@ -178,4 +179,3 @@ class DBHelper {
   } */
 
 }
-
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoia2FydGVyaW5hIiwiYSI6ImNqanNsZDB5YzZncDEzcXRlMHhqM3JyZGwifQ.5IhluUwiQmo37-N9xLB5XA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
