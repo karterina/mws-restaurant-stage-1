1aaaf41bfaf934b57fe31eb32314fa437eba3550
Ekaterina Karelova <katekarelova@gmail.com>
2018-07-24 22:56:32 +0300

    Fix: headings order

--- js/main.js
+++ js/main.js
@@ -164,7 +164,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = `a photo of ${restaurant.name} restaurant`;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
